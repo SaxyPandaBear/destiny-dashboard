1f70211493636905480981f7a3386efefff04887
Andrew Huynh <ahuynh11@gmu.edu>
2017-08-09 19:35:41 -0400

    started working on grimoire card

--- angular/src/app/cards/grimoire/grimoire.component.ts
+++ angular/src/app/cards/grimoire/grimoire.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SharedApp} from 'app/shared/services/shared-app.service';
+import {AccountGrimoireService} from 'app/bungie/services/destiny/account-grimoire.service';
+import {CardComponent} from '../_base/card.component';
+import {DestinyMembership} from "app/bungie/services/user/user.interface";
+import {SharedBungie} from "../../bungie/shared-bungie.service";
 
 @Component({
-  selector: 'app-grimoire',
-  templateUrl: './grimoire.component.html',
-  styleUrls: ['./grimoire.component.scss']
+    selector: 'app-grimoire',
+    templateUrl: './grimoire.component.html',
+    styleUrls: ['./grimoire.component.scss']
 })
-export class GrimoireComponent implements OnInit {
+export class GrimoireComponent extends CardComponent {
+    CARD_DEFINITION_ID = 7;
 
-  constructor() { }
+    private selectedMembership: DestinyMembership;
 
-  ngOnInit() {
-  }
+    constructor(public sharedApp: SharedApp, private sharedBungie: SharedBungie, private grimoireService: AccountGrimoireService) {
+        super(sharedApp);
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+
+        this.selectedMembership = this.sharedBungie.destinyMemberships[this.sharedApp.userPreferences.membershipIndex];
+
+        this.grimoireService.getAccountGrimoire(this.selectedMembership).then(data => console.log(data));
+
+    }
 
 }
